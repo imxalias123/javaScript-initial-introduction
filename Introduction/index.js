7fd9454d6a290bf69d3adb898f22a373b58a6a26
imxalias123 <sailaxmikomirineni@gmail.com>
2023-09-30 11:32:02 +0530

    javascript initial commit

---
+++ Introduction/index.js
@@ -0,0 +1,6 @@
+//console.log(document.getElementById('heading'))
+
+function changeStyle(){
+document.getElementById('heading').textContent='scriptToSoft-Technologies';
+document.getElementById('heading').style.color='red';
+}
